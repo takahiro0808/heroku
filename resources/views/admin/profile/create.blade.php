a0af3c210e847d6e902d0831059dbdf494de70b6
EC2 Default User <ec2-user@ip-172-31-17-242.us-east-2.compute.internal>
2020-06-08 02:09:48 +0000

    heroku課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -40,7 +40,7 @@
                         </div>
                     </div>
                     {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
+                    <input type="submit" class="btn btn-primary" value="更新OK">
                 </form>
             </div>
         </div>
